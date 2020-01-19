93e0e389bd71e6b47bd20277097848f8acac321c
Justin Im <20imj68@myahs.org>
2020-01-18 16:09:19 -0800

    ?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/VisionTestOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/VisionTestOP.java
@@ -1,4 +1,25 @@
-package org.firstinspires.ftc.teamcode.auto;
+package org.firstinspires.ftc.teamcode.Auto;
 
-public class VisionTestOP {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Subsystems.SkystoneContour;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name = "pepega", group = "pepehands")
+public class VisionTestOP extends LinearOpMode {
+    public SkystoneContour vision;
+    public OpenCvCamera phoneCam;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+//        phoneCam.openCameraDevice();
+//        phoneCam.setPipeline(vision);
+        waitForStart();
+        phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -1,4 +1,148 @@
 package org.firstinspires.ftc.teamcode.subsystems;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Drivetrain {
+    private DcMotor topRight;
+    private DcMotor bottomRight;
+    private DcMotor topLeft;
+    private DcMotor bottomLeft;
+    private Telemetry telemetry;
+    private static final int TICKS_PER_ROTATION = 1440;
+    private static final int WHEEL_DIAMETER = 4;
+    private static final double BOT_DIAMETER = 17.5;
+    private static final double BOT_CIRCUMFERENCE = Math.PI*BOT_DIAMETER;
+    private ElapsedTime timeX;
+    public Drivetrain(DcMotor tr, DcMotor br, DcMotor tl, DcMotor bl, Boolean isAuto, Telemetry t) {
+        this.topLeft = tl;
+        this.bottomRight = br;
+        this.topRight = tr;
+        this.bottomLeft = bl;
+        this.telemetry = t;
+
+        if(isAuto) {
+            topLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            topRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            bottomLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            bottomRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            topLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            topRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            bottomLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            bottomRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            topRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            bottomRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            topLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            bottomLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+    }
+    //김정은
+    public void controls(Gamepad gp) {
+        //TODO: Code Mecanum Bullshit
+        float x = (float)(Math.pow(gp.left_stick_y, 3));
+        float y = (float)(Math.pow(-gp.left_stick_x, 3));
+        float z = (float)(Math.pow(-gp.right_stick_x, 3));
+        if (gp.left_trigger != 0) {
+            x /=3;
+            y /=3;
+            z/= 3;
+        }
+        bottomLeft.setPower(-((x)+(y)+(-z)));
+        topLeft.setPower(-((x)+(-y)+(-z)));
+        bottomRight.setPower(-((-x)+(y)+(-z)));
+        topRight.setPower(-((-x)+(-y)+(-z)));
+    }
+
+    public void drive(double distance, double power) {
+        //TODO: Write method for driving
+        //This code is written such that forward is positive.
+        double position  = calculateTicks(distance);
+        telemetry.addLine("Moved with position ticks: " + position);
+        motorDrive(bottomLeft, position, power);
+        motorDrive(bottomRight, position, power);
+        motorDrive(topLeft, position, power);
+        motorDrive(topRight, position, power);
+        jigglypuff();
+    }
+
+    public void strafe(double distance, double power) {
+        //TODO: Write code for strafing
+        //This code is written such that right is positive.
+        double position  = calculateTicks(distance);
+        telemetry.addLine("Moved with position ticks: " + position);
+        motorDrive(bottomLeft, position, power);
+        motorDrive(bottomRight, -position, power);
+        motorDrive(topLeft, -position, power);
+        motorDrive(topRight, position, power);
+        jigglypuff();
+    }
+
+    public void turn(double degrees, double power) {
+        //TODO: Write method for turning
+        double rotations = degrees / 360 / 2.67;
+        double position = calculateTicksRot(rotations * BOT_CIRCUMFERENCE);
+        motorDrive(bottomLeft, -position, power);
+        motorDrive(bottomRight, position, power);
+        motorDrive(topLeft, -position, power);
+        motorDrive(topRight, position, power);
+        jigglypuff();
+        telemetry.addLine("Moved with position ticks: " + position);
+    }
+
+    public void REEEEEEE() {
+        topLeft.setPower(1);
+        bottomRight.setPower(1);
+        topRight.setPower(1);
+        bottomLeft.setPower(1);
+    }
+
+    private void motorDrive(DcMotor motor, double ticks, double power) {
+        //TODO: MotorDrive
+        motor.setTargetPosition((int) ticks);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setPower(power);
+    }
+
+    private double calculateTicksRot(double inches) {
+        return (inches / WHEEL_DIAMETER) * TICKS_PER_ROTATION;
+    }
+
+    private double calculateTicks(double inches) {
+        return (inches / (WHEEL_DIAMETER * Math.PI) * TICKS_PER_ROTATION);
+    }
+
+    private void jigglypuff() {
+        while ((topLeft.isBusy() && topRight.isBusy() && bottomLeft.isBusy()) || (topLeft.isBusy() && topRight.isBusy() && bottomRight.isBusy()) ||
+                (topLeft.isBusy() && bottomLeft.isBusy() && bottomRight.isBusy()) || (topRight.isBusy() && bottomLeft.isBusy() && bottomRight.isBusy())) {
+            //do nothing
+        }
+        //I'm not sure if it's good practice to reset encoders every time we move - this may slow things down and we may have to change this in order to squeeze a few extra seconds out of auto in the future
+        bottomRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        topRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bottomLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        topLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void update() {
+        telemetry.addData("BL: ", bottomLeft.getCurrentPosition());
+        telemetry.addData("TL: " , topLeft.getCurrentPosition());
+        telemetry.addData("BR: " , bottomRight.getCurrentPosition());
+        telemetry.addData("TR: " , topRight.getCurrentPosition());
+        telemetry.update();
+    }
+
+    public void residentSleeper(int ms) {
+        timeX = new ElapsedTime();
+        timeX.reset();
+        while (timeX.milliseconds() < ms){
+            continue;
+        }
+    }
+
+
 }
